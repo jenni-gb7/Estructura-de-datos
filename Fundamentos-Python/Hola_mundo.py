0c7f0f14fd3ad5d4ea86593eb9fa70032c357567
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-10 22:19:53 -0600

    Se creo el archivo tipo de datos y se le hizo una modificacion

---
+++ Fundamentos-Python/Hola_mundo.py
@@ -0,0 +1,9 @@
+# Jennifer Marlene Gutierrez Beteta
+# 7 de septiembre de 2024
+# En este archivo se muestra en consola el mensaje "Hola mundo".
+
+print("Hola mundo")
+
+#Notas:
+#Ya no se necesita utilizar punto y coma.
+#imprime el mensaje de "Hola mundo"
