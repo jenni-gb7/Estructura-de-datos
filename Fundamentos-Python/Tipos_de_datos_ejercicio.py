dcb3f7aa801b26168616019416e0fa793ebb5676
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-14 16:36:11 -0600

    Se creo el archivo tipo de datos y se le hizo una modificacion

---
+++ Fundamentos-Python/Tipos_de_datos_ejercicio.py
@@ -0,0 +1,55 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+14 de octubre de 2024
+Descripción:
+Realizar un programa que sera el ejercicio de la leccion Tipos_de_datos, el cual mostrará nuestros gastos diarios pero en este caso solo seran del los dias lunes y martes.
+'''
+
+# Notas:
+
+
+print("*** Gastos diarios***")      #Se imprime el titulo.
+
+#cadena de texto
+var_dia = "lunes"           #Se declara la cadena dia.
+print("Dia:", var_dia)     #Se imprime la cadena dia.
+
+var_pasaje = 20                #Se declara la variable de tipo entero pasaje.
+print("Pasaje:", var_pasaje)     #Se imprime la variable de tipo entero pasaje.
+
+var_comida = 135.5                #Se declara la variable de tipo decimal comida.
+print("Comida:", var_comida)     #Se imprime la variable de tipo decimal comida.
+
+var_otros = 34.5                #Se declara la variable de tipo decimal otros gastos.
+print("Otros gastos:", var_otros)     #Se imprime la variable de tipo decimal otros gastos.
+
+var_booleano= True                      #Se declara la variable de tipo booleano.
+print("¿Fue mayor a mi presupuesto?:", var_booleano)    #Se imprime la variable de tipo booleano.
+print()
+
+print("*** Gastos diarios***")      #Se imprime el encabezado
+
+#cadena de texto
+var_texto_dia = "martes"                #Se declara la cadena dia.
+print("Dia:", var_texto_dia)     #Se imprime la cadena dia.
+
+var_pasaje = 12               #Se declara la variable de tipo entero pasaje.
+print("Pasaje:", var_pasaje)     #Se imprime la variable de tipo entero pasaje.
+
+var_comida = 45.5                #Se declara la variable de tipo decimal comida.
+print("Comida:", var_comida)     #Se imprime la variable de tipo decimal comida.
+
+var_otros = 4.5                #Se declara la variable de tipo decimal otros gastos.
+print("Otros gastos:", var_otros)     #Se imprime la variable de tipo decimal otros gastos.
+
+var_booleano= False                      #Se declara la variable de tipo booleano.
+print("¿Fue mayor a mi presupuesto?:", var_booleano)    #Se imprime la variable de tipo booleano.
+
+#Notas :
+"""En este caso el programa te muestra:
+- El dia.
+- El pasaje.
+- La comida.
+- Otros gastos.
+- Y la pregunta.
+"""
