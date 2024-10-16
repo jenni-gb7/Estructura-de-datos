6c505d9a11aaa4889269f55538795ce8361721e6
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-16 15:56:47 -0600

    Se creo el archivo operadores logicos.

---
+++ Operadores de Python/Operadores_logicos.py
@@ -0,0 +1,9 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+16 de octubre de 2024
+Descripción:
+Se pide una expresion y se debe convertir en True y False
+'''
+
+expresion1=input("Ingresa si/ no:")
+expresion2=input("Ingresa si/ no:")
