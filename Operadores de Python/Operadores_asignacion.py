758cdcc051d4d4e44fc5c1c91ccbd588e74372dc
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-16 15:55:05 -0600

    Se creo el archivo operadores asignacion.

---
+++ Operadores de Python/Operadores_asignacion.py
@@ -0,0 +1,27 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+15 de octubre de 2024
+Descripción:
+Operadores asignacion
+
+'''
+#Asignacion multiple
+
+v1,v2 =5,10
+print(v1,v2)
+v3,v4=9.14,"Chuy"
+print(v3,v4)
+v5,v6=v1+v2,v1+-v2
+print(v5,v6)
+
+#Asignacion concatenada
+v7=v8=v9=10
+print(v7,v8,v9)
+
+#Intercambio de variables
+v10,v11= "Alberto","geto"
+v11,v10=v10,v11
+print(v10,v11)
+
+
+nombre,apellido=input("ingresa su nombre:"),input("ingresa su apellido:")       #Se ahorra codigo y se imprimen uno tras otro
