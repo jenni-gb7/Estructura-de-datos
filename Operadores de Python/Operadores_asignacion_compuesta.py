ad2272d32780424af97d9a67a316944ad8aefd2e
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-16 16:02:25 -0600

    Se creo el archivo operadores asignacion compuesta.

---
+++ Operadores de Python/Operadores_asignacion_compuesta.py
@@ -0,0 +1,36 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+16 de octubre de 2024
+Descripción:
+Se utilizan para comparar 2 valores
+'''
+#Nota: pendiente modificar
+num1= input("ingresa su numero:")
+num2= input("ingresa su numero:")
+
+num1=int(num1)
+num2=int(num2)
+
+num1 +=3
+print(num1)
+num2-=5
+print(num1)
+num1 *=2
+print(num1)
+num2 /=4
+print(num2)
+
+#////////
+
+
+num1=8
+num2=11
+
+num1+=num2
+num1*=num1
+num1-=num2
+num1 +=3
+num1 /=2
+
+print(f"el resultado es: {num1} {num2}")          #Imprimir el resultado final
+
