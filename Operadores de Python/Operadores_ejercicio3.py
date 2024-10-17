35907da4170dd2e909317fa543cd534079dcc673
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-17 16:18:47 -0600

    Se creo el archivo operadores logicos (AND).

---
+++ Operadores de Python/Operadores_ejercicio3.py
@@ -0,0 +1,9 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+17 de octubre de 2024
+Descripción:
+Ejercicio 3.
+'''
+num1=input("Ingresa un numero:")
+num1=int(num1)
+print(f"¿El numero {num1} esta entre 10 y 15? {num1 >= 10 and num1 <= 15}")
