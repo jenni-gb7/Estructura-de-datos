55d9647f43f4b9c4aa7039fa0813b94961ab2d6d
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-21 21:31:51 -0600

    Se creó el archivo entrada de datos por consola.

--- Fundamentos-Python/Entrada_consola.py
+++ Fundamentos-Python/Entrada_consola.py
@@ -1,6 +1,7 @@
 '''
 Jennifer Marlene Gutierrez Beteta
-1 de octubre de 2024
+18 de octubre de 2024
+Descripción:
 Entrada de datos por consola para interacturar con el usuario con valores dinámicos.
 '''
 
