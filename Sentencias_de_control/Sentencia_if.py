dd56e515c47c6a09bca6caeb1ce53abac0ff594e
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-24 16:09:18 -0600

    Se creo el archivo Sentencia if.

---
+++ Sentencias_de_control/Sentencia_if.py
@@ -0,0 +1,19 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+23 de octubre de 2024
+Descripción:
+Sentencia if.
+Programa para determinar la mayoria de edad.
+'''
+
+#Programa para determinar la mayoria de edad.
+
+#if condicion:
+    #Condicion a ejecutar.
+    #La condicion es verdadera.
+
+edad= input("Ingresa tu edad:")
+edad=int(edad)
+
+if int(edad>=18):
+    print("es mayor de edad")
