cf3e9688a74d471287884754df2020c0124363e6
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-28 23:36:12 -0600

    Se creó el archivo Operadores ejercicio #3.

---
+++ Operadores de Python/Operadores_ejercicio_3.py
@@ -0,0 +1,20 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+15 de octubre de 2024
+Descripción:
+Ejercicio 3.
+Este programa determinará si un usuario se autentifica correctamente con su usuario y contraseña.
+'''
+
+#Declarar las variables de usuario y contraseña-
+USUARIO_1= "alumno"
+CONTRASEÑA_1 = "tercero"
+
+print("*** Sistema de auteuntificación ***")
+print(" ")
+usuario = (input("Ingresa tu usuario:"))            #Pedir usuario.
+contraseña = (input("Ingresa tu contraseña:"))      #Pedir contraseña.
+
+
+print(f"¿El acceso es correcto? {usuario == USUARIO_1 and contraseña == CONTRASEÑA_1}")     #Hacer la comparación si el usuario y la contraseña son iguales.
+
