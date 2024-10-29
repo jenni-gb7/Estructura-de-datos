4b669d3923a90906424d26040fbb840aeb2320ff
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-28 21:24:08 -0600

    Se creó el archivo  Operadores lógicos.

--- Operadores de Python/Operadores_logicos.py
+++ Operadores de Python/Operadores_logicos.py
@@ -2,18 +2,25 @@
 Jennifer Marlene Gutierrez Beteta
 16 de octubre de 2024
 Descripción:
-Se pide una expresion y se debe convertir en True y False
+Operadores Lógicos.
+Se pide una expresión y se debe convertir en True y False
 '''
 
-expresion1=input("Ingresa la expresion:")
-expresion1=expresion1.lower()=="si"         #condicion para convertir de True y False
+expresion1=input("Ingresa la expresión:")   #Se solicita una expresión.
+expresion1=expresion1.lower()=="si"         #Condición para convertir de True y False
 print(expresion1)
 
-expresion2=input("Ingresa la expresion:")
-expresion2=expresion2.lower()=="si"
+expresion2=input("Ingresa la expresión:")   #Se solicita una expresión.
+expresion2=expresion2.lower()=="si"         #Condición para convertir de True y False
 print(expresion2)
 
-print(f"¿Ambos fueron si?: {expresion1 and expresion2}")
-print(f"¿Ambos fueron si?: {expresion1 or expresion2}")
-print(f"¿Ambos fueron si?: {not expresion1}")
+#NOTA:
+#Si la expresión es "si", imprimirá True.
+#Si la expresión es "no", imprimirá False.
+
+#Se hacen las comparaciones logicas and, or y not.
+print(f"¿Ambos fueron si?: {expresion1 and expresion2}")    #Comparación si las expresiones son iguales.
+print(f"¿Ambos fueron si?: {expresion1 or expresion2}")     #Comparación si al menos una expresión es si.
+print(f"¿Ambos fueron si?: {not expresion1}")   #Comparación que imprime lo contrario a lo que se ingresó.
+#Si se ingresa "si" el resultado sera False y si se ingresa "no# el resultado sera True.
 print(f"¿Ambos fueron si?: {not expresion2}")
