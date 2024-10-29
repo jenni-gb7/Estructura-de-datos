cc865d79330944d5bdf7568d8c49ccec5ee22971
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-28 21:11:49 -0600

    Se creo el archivo Operadores Aritmeticos.

--- Operadores de Python/Operadores_aritmeticos.py
+++ Operadores de Python/Operadores_aritmeticos.py
@@ -6,14 +6,20 @@ Operadores aritmeticos
 
 '''
 #NOTAS:
-usuario1= input("ingresa su numero:")
-usuario2= input("ingresa su numero:")
+#Se solicitan 2 números:
 
-usuario1=int(usuario1)
+usuario1= input("ingresa su número:")   #Se se solicita un número.
+usuario2= input("ingresa su número:")
+
+usuario1=int(usuario1)                  #Se hace la conversión de cadena a entero.
 usuario2=int(usuario2)
 
-multi= usuario1 / usuario2
 
-print(f"El resultado : {multi: .2f}")   #Division con 2 decimales
-print(usuario1 // usuario2)             #Division
-print(usuario1 * usuario2)              #Multiplicación
+print(usuario1 + usuario2)             #Imprime la suma de los 2 números.
+print(usuario1 - usuario2)             #Imprime la resta de los 2 números.
+print(usuario1 * usuario2)             #Imprime la multiplicación de los 2 números.
+print(usuario1 / usuario2)             #Imprime la división de los 2 números con un solo decimal.
+print(usuario1 ** usuario2)            #Se eleva la variable usuario1, las veces que sea la variable usuario2.
+print(usuario1 // usuario2)            #Imprime la división de los 2 números, sin decimales.
+divi= usuario1 / usuario2
+print(f"El resultado : {divi: .2f}")   #Imprime la division con 2 decimales
