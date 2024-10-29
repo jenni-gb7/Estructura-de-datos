cc172cf14e46fcbbeba62ae4fbc8befef2d9eed3
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-28 22:47:45 -0600

    Se creó el archivo Sentencia If elif else.

--- Sentencias_de_control/Sentencia_if_elif_else.py
+++ Sentencias_de_control/Sentencia_if_elif_else.py
@@ -7,21 +7,21 @@ Programa que determina si un numero es par e impar.
 '''
 
 #If elif else condición:
-#Tiene que buscar en que condición entrar.
+#Tiene que buscar en que condición entrar, sino se cumple la primera, se va con la segunda y asi sucesivamente hasta llegar al else.
 
-num1= input("Ingresa tu edad:")
-num1=float(num1)
+num1= input("Ingresa tu edad:")     #Se ingresa la edad.
+num1=int(num1)                      #Conversión de cadena a entero.
 
-if num1 <= 14:
+if num1 <= 14:                                      #Condición si el número es menor a 14.
     print("Pertenece a niños y adolescentes.")
-elif num1 >= 15 and num1 <=25:
+elif num1 >= 15 and num1 <=25:                      #Condición si el número es mayor igual a 15 y menor igual 25.
     print("Pertenece a jovenes.")
-elif num1 >= 26 and num1 <=45:
+elif num1 >= 26 and num1 <=45:                       #Condición si el número es mayor igual a 26 y menor igual 45.
     print("Pertenece a adultos jovenes.")
-elif num1 >= 46 and num1 <= 60:
+elif num1 >= 46 and num1 <= 60:                      #Condición si el número es mayor igual a 46 y menor igual 60.
     print("Pertenece a adultos maduros.")
 else:
-    print("Pertenece a adultos mayores.")
+    print("Pertenece a adultos mayores.")           #Condición sino pertenece a ninguno de los anteriores.
 
 
 
