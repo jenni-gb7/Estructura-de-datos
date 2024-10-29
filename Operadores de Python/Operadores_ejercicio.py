b83a3ac0a12f5b3267165ee1e42f131984e07506
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-28 21:53:56 -0600

    Se creó el archivo  Operadores ejercicio 1.

--- Operadores de Python/Operadores_ejercicio.py
+++ Operadores de Python/Operadores_ejercicio.py
@@ -8,11 +8,12 @@ Ejercicio 1.
 print("Compras mayores a $5000.00")
 print()
 
-cantidad=input("Ingresa la cantidad:")
-interes=input("¿Compra a meses sin en intereses?")
+cantidad=input("Ingresa la cantidad:")      #Se ingresa una cantidad.
+interes=input("¿Compra a meses sin en intereses?")  #Se ingresa si o no.
 
-cantidad=int(cantidad)
-interes =interes.lower()=="si"
+cantidad=int(cantidad)              #Se hace la conversión de cadena a entero.
+interes =interes.lower()=="si"      #Se hace la conversión de cadena a booleano.
 
-print(f"¿Aplica bonificacion? {cantidad > 5000 and interes}")
+print(f"¿Aplica bonificación? {cantidad > 5000 and interes }")   #Si la cantidad es menor a 5000 y si compra a meses sin interés.
+#Se imprime False, sino aplica a la bonificación y aplica si imprime True.
 
