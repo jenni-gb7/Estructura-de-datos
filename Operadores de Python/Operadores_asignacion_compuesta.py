baf994a1dd4f4265700886daeb068ec1ca12ab4c
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-28 20:48:52 -0600

    Se creó el archivo  Operadores asignación compuesta.

--- Operadores de Python/Operadores_asignacion_compuesta.py
+++ Operadores de Python/Operadores_asignacion_compuesta.py
@@ -5,24 +5,24 @@ Descripción:
 Se utilizan para comparar 2 valores
 '''
 #Nota: pendiente modificar
-num1= input("ingresa su numero:")
-num2= input("ingresa su numero:")
+num1= input("ingresa su número:")       #Se solicitan 2 números.
+num2= input("ingresa su número:")
 
-num1=int(num1)
+num1=int(num1)      #Se hace la conversión de cadena a entero.
 num2=int(num2)
 
 num1 +=3
-print(num1)
-num2-=5
-print(num1)
+print(num1)     #Imprime la suma de la variable num1 + 3.
+num2 -=5
+print(num2)     #Imprime la resta de la variable num2 - 5.
 num1 *=2
-print(num1)
+print(num1)     #Imprime el resultado de la multiplicación de la suma anterior * 2.
 num2 /=4
-print(num2)
+print(num2)     #Imprime el resultado de la división de la resta anterior / 4.
 
 #////////
 
-
+print(" ")
 num1=8
 num2=11
 
@@ -32,5 +32,5 @@ num1-=num2
 num1 +=3
 num1 /=2
 
-print(f"el resultado es: {num1} {num2}")          #Imprimir el resultado final
+print(f"el resultado es: {num1} {num2}")          #Imprime el resultado final de las operaciones anteriores.
 
