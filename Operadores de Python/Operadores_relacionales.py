8b58a8ab33eb73c6f32ab0ae3e90abaf99744d1e
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-10-28 21:02:36 -0600

    Se creó el archivo  Operadores relacionales.

--- Operadores de Python/Operadores_relacionales.py
+++ Operadores de Python/Operadores_relacionales.py
@@ -6,14 +6,18 @@ Operadores relacionales
 '''
 
 #Se solicitan dos numeros
-num1=87
-num2=42.7
+num1= input("ingresa su número:")       #Se solicitan 2 números.
+num2= input("ingresa su número:")
 
-#Se hacen las comparaciones y se toman los 2 primeros decimales
+num1=int(num1)      #Se hace la conversión de cadena a entero.
+num2=int(num2)
+
+#se toman los 2 primeros decimales con .2f.
+#Se hacen las comparaciones y como resultado True o False depende el caso, si es verdadero o falso.
 
 print(f"¿El {num1: .2f} es mayor  {num2: .2f}? {num1>num2}")
 print(f"¿El {num1: .2f} es mayor igual a {num2: .2f}? {num1>=num2}")
 print(f"¿El {num1: .2f} es igual {num2: .2f}? {num1==num2}")
 print(f"¿El {num1: .2f} es menor {num2: .2f}? {num1<num2}")
 print(f"¿El {num1: .2f} es menor igual {num2: .2f}? {num1<=num2}")
-print(f"¿El {num1: .2f} es diferente {num2: .2f}? {num1!=num2}")      #Operacion de diferencia
+print(f"¿El {num1: .2f} es diferente {num2: .2f}? {num1!=num2}")
