9cb2485e011d60da15625e7ce84b0bc119997994
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-10-29 22:15:52 -0600

    Se creo el archivo.

---
+++ Ciclos/Ciclo_Ejercicio_4_Cuenta.py
@@ -0,0 +1,18 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+28 de octubre de 2024
+Descripción:
+Ciclo ejercicio 4 cuenta.
+Este programa realizará diferentes acciones en una cuenta de banco.
+'''
+print(" ")
+print("*** Banco Azteca  ***")
+print(" ")
+
+while opcion != 0:
+    print("[0].- Salir")
+    print("[1].- Consulta saldo")
+    print("[2].- Ingresa saldo")
+    print("[3].- Retira saldo")
+
+
