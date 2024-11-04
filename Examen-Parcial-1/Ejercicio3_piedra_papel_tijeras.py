deaea2f838b9fffe6f566be10a177f2c3a5b1b40
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-11-03 20:51:46 -0600

    Se creó el archivo Ejercicio 3 del Examen parcial 1.

--- Examen-Parcial-1/Ejercicio3_piedra_papel_tijeras.py
+++ Examen-Parcial-1/Ejercicio3_piedra_papel_tijeras.py
@@ -26,6 +26,8 @@ d) Muestre en la consola la elección del jugador, del CPU y el resultado.
 
 e) Repita nuevamente el menú hasta salir.
 '''
+
+# Módulo random.
 import random
 
 # Inicializar contadores
@@ -35,19 +37,20 @@ empates_e = 0
 victorias_del_cpu = 0
 
 
-
-
+# Se declara la condición para el ciclo while, y asi poder entrar al menú.
+# Mientras la variable opción sea diferente de cero (0).
 while opcion_jugador != 0:
-    # Generar la opción de la CPU aleatoriamente (1, 2 o 3)
+    # Generar la opción de la CPU aleatoriamente (1 al 3)
     opcion_cpu = random.randint(1, 3)
 
-
+    # Se declaran los marcadores del juego.
     print(" ")
     print("*** Juego de piedra, papel o tijera ***")
     print(" ")
     print(f"Victorias del jugador: {victorias_del_jugador},",end=" ")
     print(f"Empates: {empates_e} y",end=" ")
     print(f"Victorias del CPU: {victorias_del_cpu}.",end=" ")
+    # Se declara el menú.
     print("")
     print("[1].- Piedra.")
     print("[2].- Papel.")
@@ -57,29 +60,36 @@ while opcion_jugador != 0:
 
     opcion_jugador = int(input("Ingresa una opción: "))
 
+    # La secuencia if-elif-else es la forma de "si las condiciones anteriores no son verdaderas, entonces prueba esta condición", es decir los número entrará en la condición que se adapte mejor a ellos.
 
+    # Condición: Si la opcion del jugador == opcion del cpu.
     if opcion_jugador == opcion_cpu:
             opciones = {1: "Piedra", 2: "Papel", 3: "Tijera"}
             print(f"Jugador: {opciones[opcion_jugador]}.", end=" ")
             print(f"CPU: {opciones[opcion_cpu]}.", end=" ")
+            # Entonces es un empate y se acomula en la variable.
             print("Empate.")
             empates_e += 1
+    # Condición: Si se cumplen que piedra y tijera o papel y piedra o tijera y papel, el que sea superior gana ya sea el jugador o la CPU..
     elif (opcion_jugador == 1 and opcion_cpu == 3) or (opcion_jugador == 2 and opcion_cpu == 1) or (opcion_jugador == 3 and opcion_cpu == 2):
         opciones = {1: "Piedra", 2: "Papel", 3: "Tijera"}
         print(f"Jugador: {opciones[opcion_jugador]}.", end=" ")
         print(f"CPU: {opciones[opcion_cpu]}.", end=" ")
         print("El ganador es el jugador.")
+        # Entonces es victorias del jugador y se acomula en la variable.
         victorias_del_jugador += 1
-
+    # Condición: Si se cumplen que piedra y tijera o papel y piedra o tijera y papel, el que sea superior gana, ya sea el jugador o la CPU.
     elif (opcion_cpu == 1 and opcion_jugador == 3) or (opcion_cpu == 2 and opcion_jugador == 1) or (opcion_cpu == 3 and opcion_jugador == 2):
         opciones = {1: "Piedra", 2: "Papel", 3: "Tijera"}
         print(f"Jugador: {opciones[opcion_jugador]}.", end=" ")
         print(f"CPU: {opciones[opcion_cpu]}.", end=" ")
         print("El ganador es el CPU.")
+        # Entonces es  victorias del cpu y se acomula en la variable.
         victorias_del_cpu += 1
+        # Si la opcion es == cero, el juego termina.
     elif opcion_jugador == 0:
         print(f"Fin del juego...")
-
+    # Si opcion <4, tiene que intentarlo de nuevo.
     else:
         print("La opción no es válida")
 
