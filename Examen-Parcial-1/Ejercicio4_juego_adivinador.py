767387a617fb9d04797075b989cd692dbbd65598
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-11-03 21:12:29 -0600

    Se creó el archivo Ejercicio 4 del Examen parcial 1.

--- Examen-Parcial-1/Ejercicio4_juego_adivinador.py
+++ Examen-Parcial-1/Ejercicio4_juego_adivinador.py
@@ -23,28 +23,46 @@ print(" ")
 print("*** Juego del adivinador ***")
 print(" ")
 
+# Módulo random.
 import random
 
 # Inicializar contadores
 num_intento = 0
 
-# Generar la opción de la CPU aleatoriamente (1, 2 o 3)
+# Generar la opción de la CPU aleatoriamente (1 al 100)
 num_secreto = random.randint(1, 100)
 
+# Se declara la condición para el ciclo while.
+# Mientras el número de intentos sean <= 4, es decir tiene 5 intentos.
 while num_intento <= 4:
 
+    # Se acomulan en la variable el número de intentos.
     num_intento += 1
 
     print(" ")
     print(f"Número de intento: {num_intento}.", end=" ")
     num_1 = int(input(f"Ingresa un número (1-100): "))
-    if num_1 <= num_secreto:
+
+    # Revisar que el número ingresado este en el rango de 1 a 100.
+    if num_1 <= 1 or num_1 >= 100:
+        print("Número fuera de rango. Ingresa un número entre 1-100")
+        # Salta al siguiente intento sin contar el actual.
+        num_intento = 0
+
+    # Comparar el número ingresado con el número secreto.
+    # Condición: Si la varible número es menor igual al número de intentos.
+    elif num_1 <= num_secreto:
+        # Se imprime el mensaje con una pista.
         print("El número a adivinar es mayor ")
+    # Condición: Si la varible número es mayor igual al número secreto.
     elif num_1 >= num_secreto:
+        # Se imprime el mensaje con una pista.
         print("El número a adivinar es menor ")
+        # Si la varible número es igual al número secreto.
     else:
         print(f"¡Felicitaciones!. Adivinaste el número {num_secreto} en {num_intento} intentos.")
 
+# Si no se cumple en 5 intentos, se termina y se imprime el siguiente mensaje.
 print(" ")
 print(f"Perdiste. El número secreto era: {num_secreto}.")
 
