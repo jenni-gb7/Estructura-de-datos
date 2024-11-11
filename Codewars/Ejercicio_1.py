d7f7bc248558abd72fe7675ab97ca5f00fdf73e8
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-11-11 16:43:44 -0600

    Ejercicio Calculadora por medio de funciones.

---
+++ Codewars/Ejercicio_1.py
@@ -0,0 +1,18 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+11 de noviembre de 2024.
+Descripción:
+Ejercicio 1 Codewars.
+'''
+
+# Complete la solución para que devuelva verdadero si el primer argumento (cadena) pasado termina con el segundo argumento (también una cadena).
+# Ejemplos:
+#
+# solution('abc', 'bc') # returns true
+# solution('abc', 'd') # returns false
+
+cadena_usuario1 = input("ingresa la cadena:")
+cadena_usuario2 = input("ingresa la cadena:")
+
+
+
