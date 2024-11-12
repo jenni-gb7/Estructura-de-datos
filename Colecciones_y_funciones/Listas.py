fcadf6471a8301f602f0f5cf512a0ae9955f98b9
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-11-12 16:25:16 -0600

    Ejercicio 1 Listas.

---
+++ Colecciones_y_funciones/Listas.py
@@ -0,0 +1,54 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+12 de noviembre de 2024.
+Descripción:
+Listas.
+'''
+
+# Crear lista.
+'''
+alumnos = ["Addi", "Jesus","Juan"]
+alumnos.append("Hector")
+
+print(alumnos)
+print(alumnos[1])
+alumnos.append("Tania")
+print(alumnos)
+for alumnos in alumnos:
+    print(alumnos, end =",")'''
+
+
+#//////////////////////////
+alumnos = []
+alumnos.append("Rosalinda")
+alumnos.append("Juan")
+alumnos.append("Lourdes")
+alumnos.append("Tania")
+alumnos.append("Bryan")
+alumnos.append("Rebeca")
+alumnos.append("Jennifer")
+alumnos.append("Hector")
+alumnos.append("Galilea")
+alumnos.append("Patricia")
+alumnos.append("Alberto")
+alumnos.append("Addi")
+
+#print(alumnos)
+
+len(alumnos)
+print(alumnos)
+print()
+
+print("-------------------------------------------------------")
+
+alumnos.sort()
+print(alumnos)
+print()
+
+print("-------------------------------------------------------")
+
+alumnos.sort(reverse = True)
+print(alumnos)
+print()
+
+print(alumnos[11])
