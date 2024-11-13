16b24587efa05a811c4a275f424841fae41b270c
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-11-13 15:39:03 -0600

    Ejercicio 1 Listas.

---
+++ Colecciones_y_funciones/Listas_ej2_compras.py
@@ -0,0 +1,47 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+13 de noviembre de 2024.
+Descripción:
+Ejericio 2.
+'''
+
+print("*** Videos de You Tube  ***")
+
+# Se implementa la función menú.
+def menu():
+    print(" ")
+    print("*** Ménu  ***")
+    print(" ")
+    print("[0].- Salir")
+    print("[1].- Añadir productos a la lista")
+    print("[2].- Eliminar productos de la lista")
+    print(" ")
+    opcion = int(input("Ingresa una opción: "))
+    return opcion
+
+def compras(opcion):
+    if opcion == 1:
+        productos = [productos]
+        productos = input("Ingrese el nombre del producto:")
+        cantidad = [cantidad]
+        cantidad = int(input("Ingrese la cantidad del producto:"))
+
+    elif opcion == 2:
+        productos.pop()
+        print(productos)
+        print()
+
+        cantidad.pop()
+        print(cantidad)
+        print()
+
+    else:
+        print("No valido")
+    return productos,cantidad
+
+
+
+
+opcion = menu()
+productos = []
+cantidad = []
