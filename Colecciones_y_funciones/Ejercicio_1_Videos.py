69fc8cee2723838ece6bbde5644dfaa7ff58392e
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-11-13 22:52:45 -0600

    Se subio el ejercicio 1, videos de you tube.

--- Colecciones_y_funciones/Ejercicio_1_Videos.py
+++ Colecciones_y_funciones/Ejercicio_1_Videos.py
@@ -24,40 +24,37 @@ while opcion != 0:
     opcion = int(input("Ingresa una opción: "))
     if opcion  == 1:
         numero = 1
-        for videos_yt in videos_yt:
-            print(f"{numero}._ {videos_yt}")
+        for video_yt in videos_yt:
+            print(f"{numero}._ {video_yt}")
             numero += 1
     elif opcion  == 2:
+        videos_yt.sort()
+        for video_yt in videos_yt:
+            print(video_yt, end=",")
             print()
-            videos_yt.sort()
-            for videos_yt in videos_yt:
-                print(videos_yt, end=",")
-                print()
     elif opcion  == 3:
+        print()
+        videos_yt.sort(reverse = True)
+        for video_yt in videos_yt:
+            print(video_yt, end=",")
             print()
-            videos_yt.sort(reverse = True)
-            for videos_yt in videos_yt:
-                print(videos_yt, end=",")
-                print()
     elif opcion  == 4:
-            print()
-            Videos = input("Ingrese el nombre del video: ")
-            videos_yt.append(Videos)
-            print()
+        print()
+        video = input("Ingrese el nombre del video: ")
+        videos_yt.append(video)
+        print()
     elif opcion  == 5:
         print()
         anadir = int(input("Cantidad de  videos que desea añadir: "))
         contador = 1
         while contador <= anadir:
-            videos = input("Ingrese el nombre del video video:")
-            videos_yt.append(videos)
+            video = input("Ingrese el nombre del video video:")
+            videos_yt.append(video)
             contador += 1
         print()
     elif opcion  == 6:
         eliminar_video = input("Ingrese el video que desea eliminar: ")
         videos_yt.remove(eliminar_video)
-        if videos == eliminar_video:
-            eliminar_video = 0
     else:
         print()
         print("Opción incorrecta")
