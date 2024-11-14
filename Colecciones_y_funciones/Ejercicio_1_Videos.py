9faa8f76dbeee711136d67669865699745c14b39
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-11-13 22:09:44 -0600

    Se subio el ejercicio 1, videos de you tube.

--- Colecciones_y_funciones/Ejercicio_1_Videos.py
+++ Colecciones_y_funciones/Ejercicio_1_Videos.py
@@ -6,47 +6,64 @@ Descripción:
 '''
 
 print("*** Videos de You Tube  ***")
+videos_yt = []
 
-# Se implementa la función menú.
-def menu():
-    print(" ")
-    print("*** Ménu  ***")
-    print(" ")
+
+opcion = 1
+while opcion != 0:
+    
+    print()
     print("[0].- Salir")
-    print("[1].- Ver lista de videos por añadidos ")
-    print("[2].- Ordenar por orden de A-Z ")
-    print("[3].- Ordenar por orden de Z-A ")
-    print("[4].- Añadir video")
+    print("[1].- Ver listas de videos por añadido")
+    print("[2].- Ver listas de videos por orden A-Z")
+    print("[3].- Ver listas de videos por orden Z-A")
+    print("[4].- Añadir videos")
     print("[5].- Añadir varios videos")
-    print("[6].- Eliminar video")
-    print(" ")
+    print("[6].- Eliminar videos")
+    print()
     opcion = int(input("Ingresa una opción: "))
-    return opcion
-
-# Se implementa la función calculadora.
-def videos(opcion):
-    if opcion == 1:
-        print(alumnos)
-    elif opcion == 2:
-        alumnos.sort()
-        print(alumnos)
+    if opcion  == 1:
+        numero = 1
+        for videos_yt in videos_yt:
+            print(f"{numero}._ {videos_yt}")
+            numero += 1
+    elif opcion  == 2:
+            print()
+            videos_yt.sort()
+            for videos_yt in videos_yt:
+                print(videos_yt, end=",")
+                print()
+    elif opcion  == 3:
+            print()
+            videos_yt.sort(reverse = True)
+            for videos_yt in videos_yt:
+                print(videos_yt, end=",")
+                print()
+    elif opcion  == 4:
+            print()
+            Videos = input("Ingrese el nombre del video: ")
+            videos_yt.append(Videos)
+            print()
+    elif opcion  == 5:
         print()
-    elif opcion == 3:
-        alumnos.sort(reverse=True)
-        print(alumnos)
+        anadir = int(input("Cantidad de  videos que desea añadir: "))
+        contador = 1
+        while contador <= anadir:
+            videos = input("Ingrese el nombre del video video:")
+            videos_yt.append(videos)
+            contador += 1
         print()
-    elif opcion == 4:
-
-
-    elif opcion == 5:
-        resultado = numero_1 / numero_2
-    elif opcion == 6:
-        resultado = numero_1 ** numero_2
+    elif opcion  == 6:
+        eliminar_video = input("Ingrese el video que desea eliminar: ")
+        videos_yt.remove(eliminar_video)
+        if videos == eliminar_video:
+            eliminar_video = 0
     else:
-        print("Saliendo del programa")
-    return resultado
+        print()
+        print("Opción incorrecta")
+        print()
+print()
+print("Saliendo del programa...")
 
 
-# Se declaran las funciones.
-opcion = menu()
-alumnos = []
+
