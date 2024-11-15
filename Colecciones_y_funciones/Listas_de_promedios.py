410044fa3955cf830ad4b728ae3eb9dba380bef5
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-11-14 22:31:18 -0600

    Ejercicio 3, lista de promedios. corregir.

--- Colecciones_y_funciones/Listas_de_promedios.py
+++ Colecciones_y_funciones/Listas_de_promedios.py
@@ -5,41 +5,63 @@ Descripción:
 Ejericio 3, Listas de promedio.
 '''
 
-print("*** Promedios del parcial 1  ***")
+alumnos = []
+calificaciones = []
+promedios = []
+opcion= 1
+contador = 0
 
-# Se implementa la función menú.
-def menu():
+while opcion != 0:
     print(" ")
-    print("*** Ménu  ***")
+    print("*** Promedios del parcial 1  ***")
     print(" ")
     print("[0].- Salir")
     print("[1].- Ver calificación de alumno")
     print("[2].- Ver promedio de alumno")
     print("[3].- Añadir alumno")
-    print("[4].- Eliminsr alumno")
+    print("[4].- Eliminar alumno")
     print("[5].- Ver promedio grupal")
     print(" ")
     opcion = int(input("Ingresa una opción: "))
-    return opcion
-
-def promedio(opcion):
-    if opcion == 1:
-        print(calificaciones)
-        print(promedio)
+    if opcion  == 1:
+        numero_1 = 0
+        for calificacion in calificaciones:
+            print(f"Calificaciones: {calificaciones}")
+            numero_1+= 1
+            print(" ")
     elif opcion == 2:
-        productos.pop()
-        print(productos)
+        numero_2 = 0
+        for promedio in promedios:
+            print(f" {promedios[numero_2]} {promedios}")
+            numero_2 += 1
+    elif opcion == 3:
         print()
+        calificacion_electronica = float(input("Ingrese la calificación electrónica: "))
+        calificaciones.append(calificacion_electronica)
+        calificacion_derecho = float(input("Ingrese la calificación derecho: "))
+        calificaciones.append(calificacion_derecho)
+        calificacion_algebra = float(input("Ingrese la calificación algébra: "))
+        calificaciones.append(calificacion_algebra)
+        calificacion_contabilidad = float(input("Ingrese la calificación contabilidad: "))
+        calificaciones.append(calificacion_contabilidad)
+        calificacion_estructura = float(input("Ingrese la calificación estructura: "))
+        calificaciones.append( calificacion_estructura)
 
-        cantidad.pop()
-        print(cantidad)
+        contador += 1
         print()
+    elif opcion == 4:
+        eliminar_alumno = input("Ingrese el producto que desea eliminar: ")
+        numero_alumno = 0
+
+        while alumnos[numero_alumno] !=  eliminar_alumno:
+            numero_alumno += 1
+
+        alumnos.remove(eliminar_alumno)
+        del calificaciones[numero_alumno]
 
     else:
-        print("No valido")
-    return productos,cantidad
-
-
-opcion = menu()
-calificaciones = []
-promedio = []
+        print()
+        print("Opción incorrecta")
+        print()
+print()
+print("Saliendo del programa...")
