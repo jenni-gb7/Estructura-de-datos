45763715e99b0d0494cf4731b396af1b8563d141
jenni-gb7 <gutierrezbetettam95@gmail.com>
2024-11-21 16:32:41 -0600

    Tuplas Funciones.

--- Colecciones_y_funciones/Tuplas_Funciones.py
+++ Colecciones_y_funciones/Tuplas_Funciones.py
@@ -11,24 +11,20 @@ print(" ")
 
 # Se crea la función.
 
-def calificacion(calificaciones):
-    promedio_parcial = sum(calificaciones[0.2])/3
-    promedio_final = promedio_parcial + (calificaciones[3])/2
-    return (promedio_parcial, promedio_final)
+def Promedios(calificaciones):
+    Promedio_parcial = sum(calificaciones[0:3])/3
+    Promedio_final = (Promedio_parcial + calificaciones[3])/2
+    return Promedio_parcial, Promedio_final
 
-primer_parcial = float(input("Ingresa la calificación del 1 parcial:"))
-segundo_parcial = float(input("Ingresa la calificación del 2 parcial:"))
-tercer_parcial = float(input("Ingresa la calificación del 3 parcial:"))
-ordinario = float(input("Ingresa la calificación del ordinario:"))
-# Se crea una tupla.
+parcial1 = float(input("Ingresa la calificación del Parcial 1: "))
+parcial2 = float(input("Ingresa la calificación del Parcial 2: "))
+parcial3 = float(input("Ingresa la calificación del Parcial 3: "))
+ordinario = float(input("Ingresa la calificación del Ordinario: "))
+print()
 
-calificaciones = (primer_parcial, segundo_parcial, tercer_parcial, ordinario)
+calificaciones = (parcial1, parcial2, parcial3, ordinario)
 print(calificaciones)
-promedio_parcial, promedio_final = calificacion(calificaciones)
 
-if esta_aprobado:
-    print("¡Felicidades aprobaste!")
-else:
-    print("Lo siento, no aprobaste...")
+Promedio_parcial, Promedio_final= Promedios(calificaciones)
 
-print(f"(El promedio de los parciales es  : {promedio_parcial: 1.f} y el promedio final es: {promedio_final: 1.f}")
+print(f"(El promedio de los parciales es  : {Promedio_parcial: .1f} y el promedio final es: {Promedio_final: .1f}")
