5214dab73dc5cf8ee17066e1ed375be198c1c577
PERIFERICO <gutierrezbetetam)5@gmail.com>
2024-11-26 19:27:38 -0600

    Ejercicio 5 pirámides.

--- Ciclos/Ciclo_Ejercicio_5_piramide.py
+++ Ciclos/Ciclo_Ejercicio_5_piramide.py
@@ -1,40 +1,76 @@
 '''
 Jennifer Marlene Gutierrez Beteta
-06 de noviembre de 2024.
+07 de noviembre de 2024.
 Descripción:
-Ciclo for.
+#Este programa imprime una pirámide de caracteres '*' en cuatro formas diferentes según la cantidad de filas.
 '''
 
-#Ejercicio 1.
-n_fila = int(input("Ingrese un número de fila: "))
-contador= "*"
+'''Escribe un programa de nombre Ciclos_ej5_piramide.py que realice lo siguiente:
+Este programa imprime una pirámide de caracteres '*' de cuatro formas:
+1)
+*
+**
+***
 
-for i in range(1,n_fila+1):
-    contador = "*" * i
-    print(f" {contador}")
+2)
+***
+**
+*
+
+3)
+  *
+ ***
+*****
+
+4)
+  *
+ **
+***
+
+En donde el usuario ingresa el número de filas. Para ello:
+a) Solicite el número de filas de la pirámide.
+b) Muestre los tres tipos de pirámides utilizando la lógica adecuada.
+'''
+# Se olicita al usuario el número de filas para las pirámides.
+Numero_filas = int(input("Ingrese el número de filas: "))
+
 print()
+print("Pirámide 1:")
+for j in range (1,Numero_filas + 1):
+    asterisco = "*" * j
+    print(f"{asterisco}")
+print("/////////////////////////////////////////////")
 
-#Ejercicio 2.
-n_fila = int(input("Ingrese un número de fila: "))
-contador= "*"
-
-for j in range(1,n_fila+1):
-    contador = "*" * n_fila
-    n_fila = n_fila - 1
-    print(f" {contador}")
 print()
-
-#Ejercicio 3.
-n_fila = int(input("Ingrese un número de fila: "))
-
-contador= n_fila
-
-for k in range(1,n_fila+1):
-    espacio = " " * k
-    contador = "*" * contador
-    print(f"{espacio} {contador}")
+print("Pirámide 2:")
+contador = Numero_filas
+for i in range (1,Numero_filas+ 1):
+    asterisco = "*" * contador
+    print(f"{asterisco}")
     contador -= 1
-    print()
+print("/////////////////////////////////////////////////////////////")
 
-#Ejercicio 4.
+print()
+print("Pirámide 3:")
+contador = Numero_filas
+contador_2 = 0
+for k in range (1,Numero_filas+ 1):
+    espacio = " " * contador
+    asterisco = "*" * (k + contador_2)
+    print(f"{espacio}{asterisco}")
+    contador -= 1
+    contador_2 += 1
+print("///////////////////////////////////////////////////////")
 
+
+print()
+print("Pirámide 4:")
+contador = Numero_filas
+contador_2 = 0
+for l in range (1,Numero_filas + 1):
+    espacio = " " * contador
+    asterisco = "*" * l
+    print(f"{espacio}{asterisco}")
+    contador -= 1
+    contador_2 += 1
+print("//////////////////////////////////////////////////////////")
