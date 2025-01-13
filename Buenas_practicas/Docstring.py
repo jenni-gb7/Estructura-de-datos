28fba2236ed2dc95792d680360da4b55c94c9bb7
jenni-gb7 <gutierrezbetettam95@gmail.com>
2025-01-13 17:29:51 -0600

    docstring

---
+++ Buenas_practicas/Docstring.py
@@ -0,0 +1,55 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+09 de enero de 2025.
+Descripción:
+Ejercicio 2, Parcial 3.
+'''
+
+def menu() :
+    '''
+    Esta función muestra el menú del programa.
+    :return: Devuelve la opción seleccionada.
+    '''
+    print("[1].- Convertir a entero")
+    print("[2].- Convertir a flotante")
+    print("[0].-Salir")
+
+    opcion = int(input("Ingresa una de las opciones: "))
+    return opcion
+#
+def cadena_a_entero(cadena: str) -> int | None:
+    '''
+    :param cadena: Es la cadena a convertir a entero.
+    :return: Devuelve el entero de lo contrario devuelve None.
+    '''
+    no_guiones = cadena.count("-")
+    revisar_cadena = cadena.lstrip("-")
+    if revisar_cadena.isnumeric() and no_guiones in(0,1) :
+        return  int(cadena)
+    else:
+        return None
+#
+def cadena_a_flotante(cadena):
+    def cadena_a_flotante(cadena):
+        no_puntos = cadena.count(".")
+        no_guiones = cadena.count("-")
+        revisar_cadena = cadena.lstrip("-").replace(".", "")
+
+        if revisar_cadena.isnumeric() and no_guiones in (0, 1) and no_puntos in (0, 1):
+            return float(cadena)
+        else:
+            return None
+opcion = menu()
+
+while opcion != 0:
+    if opcion == 1:
+        num_cadena = input("Ingresa un número a converir: ")
+        num_cadena = cadena_a_entero(num_cadena)
+        print(f"el resultado final es {num_cadena}")
+    else:
+        if opcion == 2:
+            num_cadena = input("Ingresa un número a converir: ")
+            num_cadena = cadena_a_flotante(num_cadena)
+            print(f"el resultado final es {num_cadena}")
+
+print("Salio del programa")
