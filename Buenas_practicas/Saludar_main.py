25d41f2c7701edbaa04a773241c949fbe32c886f
jenni-gb7 <gutierrezbetettam95@gmail.com>
2025-01-13 17:27:36 -0600

    Saludar modulo.

---
+++ Buenas_practicas/Saludar_main.py
@@ -0,0 +1,13 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+13 de enero de 2025.
+Descripción:
+Ejercicio 2, Parcial 3.
+'''
+
+# Importar el módulo.
+from Saludar_modulo import Saludar
+
+if __name__ == '__main__':
+    Nombre = input("Ingresa un nombre: ")
+    Saludar(Nombre)
