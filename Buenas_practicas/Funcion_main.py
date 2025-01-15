7fa82c8e82cd7ecb266759454ca3b0f9d89aa9ec
jenni-gb7 <gutierrezbetettam95@gmail.com>
2025-01-15 15:49:08 -0600

    Corregido.

--- Buenas_practicas/Funcion_main.py
+++ Buenas_practicas/Funcion_main.py
@@ -4,6 +4,9 @@ Jennifer Marlene Gutierrez Beteta
 Descripción:
 Ejercicio 2, Parcial 3.
 '''
+from tkinter.font import names
+
+
 def Menu() :
     '''
     Esta función muestra el menú del programa.
@@ -34,22 +37,26 @@ def Resta(cadena: str) -> float | None:
 
 opcion = Menu()
 
-while opcion != 0:
-    if opcion == 1:
-        Num1_Suma = input("Ingresa el primer número a sumar: ")
-        Num1_Suma = Suma(Num1_Suma)
-        Num2_Suma = input("Ingresa el segundo número a sumar:")
-        Num2_Suma = Suma(Num2_Suma)
-        Resultado_Suma = Num1_Suma + Num2_Suma
-        print(f"el resultado de la suma de {Num1_Suma} y {Num2_Suma} es {Resultado_Suma}")
-    elif opcion == 2:
-        Num1_Resta = input("Ingresa el primer número a sumar: ")
-        Num1_Resta = Resta(Num1_Resta)
-        Num2_Resta = input("Ingresa el segundo número a sumar:")
-        Num2_Resta = Resta(Num2_Resta)
-        Resultado_Resta = Num1_Resta - Num2_Resta
-        print(f"el resultado de la resta de {Num1_Resta} y {Num2_Resta} es {Resultado_Resta}")
-    else:
-        print("Opción no valida")
+def main() -> None:
+    while opcion != 0:
+        if opcion == 1:
+            Num1_Suma = input("Ingresa el primer número a sumar: ")
+            Num1_Suma = Suma(Num1_Suma)
+            Num2_Suma = input("Ingresa el segundo número a sumar:")
+            Num2_Suma = Suma(Num2_Suma)
+            Resultado_Suma = Num1_Suma + Num2_Suma
+            print(f"el resultado de la suma de {Num1_Suma} y {Num2_Suma} es {Resultado_Suma}")
+        elif opcion == 2:
+            Num1_Resta = input("Ingresa el primer número a sumar: ")
+            Num1_Resta = Resta(Num1_Resta)
+            Num2_Resta = input("Ingresa el segundo número a sumar:")
+            Num2_Resta = Resta(Num2_Resta)
+            Resultado_Resta = Num1_Resta - Num2_Resta
+            print(f"el resultado de la resta de {Num1_Resta} y {Num2_Resta} es {Resultado_Resta}")
+        else:
+            print("Opción no valida")
+    print("Salio del programa")
 
-print("Salio del programa")
+
+if __name__ == '__main__':  # indica si el código es el que se ejecuta.
+    main()
