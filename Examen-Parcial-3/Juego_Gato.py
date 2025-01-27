09b8579963b84838bcbdaabc5c90cdd85f1dcdd2
PERIFERICO <gutierrezbetetam)5@gmail.com>
2025-01-26 21:26:25 -0600

    terminar.

---
+++ Examen-Parcial-3/Juego_Gato.py
@@ -0,0 +1,17 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+22 de enero de 2025.
+Descripción:
+Examen Parcial 3: Juego del ahorcado.
+'''
+
+def Num_de_jugadores():
+    print()
+    print("Bienvenido al juego del Gato")
+    print("[1].- Jugar con 1 jugador")
+    print("[2].- Jugar con 2 jugadores")
+    print("[0].- Salir")
+
+    selccion = int(input("Ingresa una de las opciones: "))
+    return selccion
+
